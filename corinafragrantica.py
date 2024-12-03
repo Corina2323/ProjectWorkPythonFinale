7bb587b7c51a2945e71fd4fa24e5f55ebf14f698
Corina2323 <corina.steflea@yahoo.com>
2024-12-03 16:29:48 +0100

    Varia

---
+++ corinafragrantica.py
@@ -0,0 +1,25 @@
+Esame Project Work
+Aprire il dataset di formato .csv
+Controllo della correttezza del dataset
+Controllo valori nulli
+Verificare se ci sono ed eliminarli
+Controllo duplicati
+Verificare se ci sono ed eliminarli
+Controllo coerenza stringhe
+Se una colonna ha valori stringa verificare se ci sono errori ed eventualmente correggerli
+Analisi dati
+Calcolo media
+Calcolo mediana
+Calcolo varianza (std)
+Preprocessing dataset
+Normalizzazione (se necessaria)
+Conversione stringhe->numero
+Preparazione dei dati
+Dividere features dalle labels
+Split del dataset in training set e test set
+Modello di Machine Learning
+Scelta del modello da utilizzare
+Addestramento sul training set
+Predizioni sul test set
+Valutazione del modello
+Calcolare l’accuratezza del modello tra le labels ed il test set
